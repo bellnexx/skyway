91122bff3897970d9e9e81b3121191d87d3cd5ef
bellnexx <32121687+bellnexx@users.noreply.github.com>
2025-05-01 17:33:20 +0530

    get room name from input

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ const token = new SkyWayAuthToken({
 
     const buttonArea = document.getElementById("button-area");
     const remoteMediaArea = document.getElementById("remote-media-area");
-    let roomNameInput = "sss";//"TAXIREADER-BROADCAST"//document.getElementById("room-name");
+    let roomNameInput = document.getElementById("room-name");//"TAXIREADER-BROADCAST"//document.getElementById("room-name");
     const myId = document.getElementById("my-id");
     const joinButton = document.getElementById("join");
     const leaveButton = document.getElementById('leave');
